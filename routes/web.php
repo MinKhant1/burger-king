f15ba013742108f6c63d4d1eeea62cab864ac69a
MinKhant1 <34791046+MinKhant1@users.noreply.github.com>
2022-10-25 19:21:11 +0630

    products done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+Route::get('/',[ProjectController::class,'index'])->name('home');
+
+
+
+Route::get('/about',function()
+{
+return view('about');
 });
+
+Route::get('/products',[ProjectController::class,'products'])->name('products');
+
+
+Route::get('/single_product',function()
+{
+    return redirect('/');
+
+});
+
+Route::get('/single_product/{id}',[ProjectController::class,'single_product'])->name('single_product');
