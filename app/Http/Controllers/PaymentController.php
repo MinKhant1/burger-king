29d2e451a0afe1156b2aad169694db84ad366b66
MinKhant1 <34791046+MinKhant1@users.noreply.github.com>
2022-10-27 23:44:18 +0630

    order done

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    //
+
+    function payment()
+    {
+        return view('payment');
+        
+    }
+}
